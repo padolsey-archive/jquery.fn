efe736443b1fe86ad10a320e0d580c5855ee19a9
James Padolsey <jamespadolsey@gmail.com>
2009-12-26 20:18:40 +0000

    s/\t/    /

--- macro/jquery.macro.js
+++ macro/jquery.macro.js
@@ -11,35 +11,35 @@
  */
 
 jQuery.macro = (function(){
-	
-	function Macro(name) {
-		
-		if ( !(this instanceof Macro) ) {
-			return new Macro(name);
-		}
+    
+    function Macro(name) {
+        
+        if ( !(this instanceof Macro) ) {
+            return new Macro(name);
+        }
     
         var recorded = this.recorded = [],
-			self = this;
-		
-		this.add = function(name, args) {
-			recorded.push({
-				name: name,
-				args: args
-			});
-		};
-		
+            self = this;
+        
+        this.add = function(name, args) {
+            recorded.push({
+                name: name,
+                args: args
+            });
+        };
+        
         jQuery.fn[name] = function() {
             
             var m, i = 0, prev = this, cur = this;
-			
+            
             while (m = recorded[i++]) {
                 cur = this[m.name].apply(prev, m.args);
-				// Returned collection must be instanceof jQuery
-				// since it will be the context for the next call
-				if ( !(cur instanceof jQuery) ) {
-					cur = prev;
-				}
-				prev = cur;
+                // Returned collection must be instanceof jQuery
+                // since it will be the context for the next call
+                if ( !(cur instanceof jQuery) ) {
+                    cur = prev;
+                }
+                prev = cur;
             }
             
             return this;
@@ -47,30 +47,30 @@ jQuery.macro = (function(){
         };
         
     }
-	
-	function register(name, fn) {
-		
-		fn = fn || proto[name];
-		
-		proto[name] = function() {
-			this.add(name, arguments);
-			return this;
-		};
-		
-	}
+    
+    function register(name, fn) {
+        
+        fn = fn || proto[name];
+        
+        proto[name] = function() {
+            this.add(name, arguments);
+            return this;
+        };
+        
+    }
     
     var proto = Macro.prototype = jQuery.extend({}, jQuery.fn),
-		excludes = Macro.excludes = { init: true };
-		
-	Macro.register = register;
+        excludes = Macro.excludes = { init: true };
+        
+    Macro.register = register;
     
     for (var i in proto)  {
 
         if ( !jQuery.isFunction(proto[i]) || i in excludes ) {
-			continue;
-		}
-		
-		register(i);
+            continue;
+        }
+        
+        register(i);
         
     }
     
